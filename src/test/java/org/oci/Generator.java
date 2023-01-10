bfc07c0ca3b7f88f4af6f5d61b1c70caf13d55e6
Hassan El-seoudy <hassan.elseoudy@gmail.com>
2023-01-10 12:55:20 +0100

    :sparkles: File Generator.

---
+++ src/test/java/org/oci/Generator.java
@@ -0,0 +1,47 @@
+package org.oci;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Generator {
+
+    public static File createFileWithEncoding(String encoding) throws IOException {
+        File newFile = new File("file_with_" + encoding + "_encoding");
+        try (FileWriter writer = new FileWriter(newFile)) {
+            writer.write("This is a test file with " + encoding + " encoding.");
+        }
+        return newFile;
+    }
+
+    public static File createFileWithContentType(String contentType) throws IOException {
+        File newFile = new File("file_with_" + contentType + "_content_type");
+        try (FileWriter writer = new FileWriter(newFile)) {
+            if (contentType.equals("application/json")) {
+                writer.write("{\"key\":\"value\"}");
+            } else {
+                return createSamplePNGImage();
+            }
+        }
+        return newFile;
+    }
+
+    private static File createSamplePNGImage() throws IOException {
+        File file = new File("path/to/sample_image.png");
+        int width = 100;
+        int height = 100;
+        BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        // fill the image with some random pixels
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                bufferedImage.setRGB(i, j, (int) (Math.random() * 0xFFFFFF));
+            }
+        }
+        ImageIO.write(bufferedImage, "png", file);
+        return file;
+    }
+
+
+}
